5cc5ff96859dc04ff2022219b8ca8bd9dd50cd17
Chris Phornroekngam <chris@HANPEN.local>
2024-02-02 12:09:47 -0600

    fixed typo and renamed

---
+++ main.py
@@ -0,0 +1,35 @@
+import csv
+import fileIO
+
+while(True):
+    
+    print("1 - HW")
+    print("2 - LAB")
+    print("0 - Exit")
+    
+
+    flag = True
+    while(flag):
+        flag = False
+        selection = int(input("Select grading operation: "))
+        number = input("Input assignment number in format XX (e.g. 05): ")
+        file_name = input("CSV of file to grade: ")
+        if(selection == 1):
+            try:
+                fileIO.importHW(file_name, number)
+                print("-------------------")
+                print("Graded successfully")
+                print("-------------------")
+            except:
+                print("!!!!!!!!!!!!!!")
+                print("File not found")
+                print("!!!!!!!!!!!!!!")
+                
+
+        elif(selection == 2):
+            fileIO.importLab(file_name, number)
+        elif(selection == 0):
+            exit()
+        else:
+            print("invalid option")
+            flag = True
